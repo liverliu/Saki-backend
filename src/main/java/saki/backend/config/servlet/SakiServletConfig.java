fda4b120558917fa8f2427df0832091c834eb7a9
liverliu <shijianliu@deepglint.com>
2016-07-15 11:53:36 +0800

    Add view

--- src/main/java/saki/backend/config/servlet/SakiServletConfig.java
+++ src/main/java/saki/backend/config/servlet/SakiServletConfig.java
@@ -1,8 +1,6 @@
 package saki.backend.config.servlet;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.*;
 
 
@@ -10,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.*;
  * Created by liverliu on 15/10/31.
  */
 @Configuration
-@EnableWebMvc
 public class SakiServletConfig extends WebMvcConfigurerAdapter {
 
     @Override
@@ -20,12 +17,12 @@ public class SakiServletConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/view/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/view/**").addResourceLocations("classpath:/templates/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setStatusCode(HttpStatus.MOVED_PERMANENTLY)
-                .setViewName("view/pages/welcome.html");
+        registry.addViewController("/").setViewName("redirect:/view/welcome.html");
     }
 }
